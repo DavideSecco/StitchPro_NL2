7520493d0be63e092fc71b09283440d26c2c4ca1
Paolo Di Ciano <135977768+paolodc5@users.noreply.github.com>
2024-08-21 17:22:44 +0200

    test su ellisse

--- prova.py
+++ prova.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x = np.zeros((500, 600)).astype(np.uint8)
+x = np.zeros((1000, 1000)).astype(np.uint8)
 Mx, My = x.shape
-c = (int(0), int(My/4))
-cv2.ellipse(x, center=c, axes=(200, 200), angle=0, startAngle=0, endAngle=35, color=255, thickness=-1)
+c = (int(0), int(720))
+value = [0, 720, 360.0, 360.0, -3.141592653589793, 1.5707963267948966]
+cv2.ellipse((0, 720), (360.0, 360.0), -3.141592653589793, 1.5707963267948966, 255)
 cv2.drawMarker(x, position=c, thickness=3, color=250)
 print(35)
 plt.imshow(x, cmap='gray')
