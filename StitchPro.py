686114c09f219da8e895a7e41f36192a891f76da
davidesecco <d.secco2000@gmail.com>
2024-09-22 18:04:03 +0200

    added data_dict visualization + json

--- StitchPro.py
+++ StitchPro.py
@@ -32,6 +32,7 @@ import argparse
 import tifffile
 from tiatoolbox.wsicore import WSIReader
 import time
+import json
 
 import matplotlib.pyplot as plt # DEBUGGING
 
@@ -460,7 +461,7 @@ if (img_file_buffer_ur is not None) & (img_file_buffer_lr is not None) & (img_fi
                 np.array(np.where(pos_axis_line_mask * x_out)).T, 1, 1)
 
             ### DEBUGGING
-            debug = False
+            debug = True
             if debug:
                 xx = images[i].copy()
                 for pointt in pos_points:
@@ -519,8 +520,33 @@ if (img_file_buffer_ur is not None) & (img_file_buffer_lr is not None) & (img_fi
             print("pos_line", data_dict[i]["pos_line"])
             # print("data_dict: ", data_dict)
 
+            # COnversion needed since you can't save a nparray in a json file
+            # data_dict[i]['image'] = data_dict[i]['image'].tolist()
+            # data_dict[i]['tissue_mask'] = data_dict[i]['tissue_mask'].tolist()
+            # data_dict[i]['tissue_mask_closed'] = data_dict[i]['tissue_mask_closed'].tolist()
+            data_dict[i]['ant_line'] = data_dict[i]['ant_line'].tolist()
+            data_dict[i]['pos_line'] = data_dict[i]['pos_line'].tolist()
+            data_dict[i]['ant_points'] = data_dict[i]['ant_points'].tolist()
+            data_dict[i]['pos_points'] = data_dict[i]['pos_points'].tolist()
+
+            # Save as json
+            with open(os.path.join(save_dir, "data_dict_vanilla.json"), "w") as file:
+                # json.dump(data_dict[i], file)
+                json.dump(dict((k, data_dict[i][k]) for k in ['ant_line', 'pos_line', 'ant_points', 'pos_points']),
+                          file,
+                          separators=(',', ': '))
+
+            # Li faccio tornare nparray perchè serve cosi dopo
+            data_dict[i]['image'] = np.array(data_dict[i]['image'])
+            data_dict[i]['tissue_mask'] = np.array(data_dict[i]['tissue_mask'])
+            data_dict[i]['tissue_mask_closed'] = np.array(data_dict[i]['tissue_mask_closed'])
+            data_dict[i]['ant_line'] = np.array(data_dict[i]['ant_line'])
+            data_dict[i]['pos_line'] = np.array(data_dict[i]['pos_line'])
+            data_dict[i]['ant_points'] = np.array(data_dict[i]['ant_points'])
+            data_dict[i]['pos_points'] = np.array(data_dict[i]['pos_points'])
+
             # Crea una figura con 3 sottotrame
-            fig, axs = plt.subplots(1, 3, figsize=(15, 5))  # 1 riga, 3 colonne
+            fig, axs = plt.subplots(2, 3, figsize=(15, 5))  # 1 riga, 3 colonne
             ax = axs.ravel()
             # Visualizza ciascuna immagine in una sottotrama
             ax[0].imshow(data_dict[i]["image"])
@@ -532,12 +558,40 @@ if (img_file_buffer_ur is not None) & (img_file_buffer_lr is not None) & (img_fi
             ax[2].imshow(data_dict[i]["tissue_mask_closed"], cmap="gray")
             ax[2].axis('off')
 
+            # Mostra la visualizzazione con le tre immagini
+            # plt.show()
+
+            # Stampo i punti
+            # aux_mask = cv2.cvtColor(aux_mask, cv2.COLOR_GRAY2RGB)
+            aux_mask = data_dict[i]["tissue_mask_closed"]
+            # Convert aux_mask from CV_32S to CV_8U
+            aux_mask = aux_mask.astype(np.uint8)
+            aux_mask = cv2.cvtColor(aux_mask, cv2.COLOR_GRAY2RGB)
+            # disegna i punti per sola visualizzazione
+            for point in data_dict[i]['ant_points']:
+                cv2.drawMarker(aux_mask, tuple(point), color=(255, 255, 255), markerType=cv2.MARKER_SQUARE,
+                               markerSize=3, thickness=2)
+
+            for point in data_dict[i]['pos_points']:
+                cv2.drawMarker(aux_mask, tuple(point), color=(155, 155, 15), markerType=cv2.MARKER_SQUARE, markerSize=3,
+                               thickness=2)
+
+            ax[3].imshow(aux_mask)
+            ax[3].set_title("Contorni")
+
+            aux_mask2 = data_dict[i]["tissue_mask_closed"]
+            ax[4].imshow(aux_mask2)
+            # Plot the line over the image
+            ax[4].axline(data_dict[i]["ant_line"][0], xy2=data_dict[i]["ant_line"][1], color='red', linewidth=2,
+                         marker='o')
+            ax[4].axline(data_dict[i]["pos_line"][0], xy2=data_dict[i]["pos_line"][1], color='blue', linewidth=2,
+                         marker='o')
+
             plt.tight_layout()
 
             plt.savefig(os.path.join(save_dir, 'data_dict_vanilla'), dpi=300)
 
-            # Mostra la visualizzazione con le tre immagini
-            plt.show()
+            plt.close('all')
 
         print(cx, cy)
 
