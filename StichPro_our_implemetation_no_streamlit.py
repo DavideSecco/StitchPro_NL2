bde5d20e9538ce343cb0583d212f3734cabdc945
davidesecco <d.secco2000@gmail.com>
2024-10-14 23:16:35 +0200

    Aggiunto il support al run su Kaggle

--- StichPro_our_implemetation_no_streamlit.py
+++ StichPro_our_implemetation_no_streamlit.py
@@ -33,7 +33,7 @@ import time
 import json
 
 import matplotlib.pyplot as plt  # DEBUGGING
-from memory_profiler import profile
+
 import gc
 
 gc.collect()
@@ -56,21 +56,6 @@ try:
 except OSError as e:
     print(f"Errore nella creazione della cartella: {e}")
 
-## App description
-start_time = time.time()
-
-## Upload images and rotate them by given angle
-if os.path.exists("/mnt/Volume/Mega/LaureaMagistrale/CorsiSemestre/A2S1/MultudisciplinaryProject/StitchPro/test-data/"):
-    img_file_buffer_ur = "/mnt/Volume/Mega/LaureaMagistrale/CorsiSemestre/A2S1/MultudisciplinaryProject/data/Dataset_00/upper_right.tif"
-    img_file_buffer_lr = "/mnt/Volume/Mega/LaureaMagistrale/CorsiSemestre/A2S1/MultudisciplinaryProject/data/Dataset_00/bottom_right.tif"
-    img_file_buffer_ll = "/mnt/Volume/Mega/LaureaMagistrale/CorsiSemestre/A2S1/MultudisciplinaryProject/data/Dataset_00/bottom_left.tif"
-    img_file_buffer_ul = "/mnt/Volume/Mega/LaureaMagistrale/CorsiSemestre/A2S1/MultudisciplinaryProject/data/Dataset_00/upper_left.tif"
-elif os.path.exists(r"C:\Users\dicia\NL2_project\datasets\test-data-corretto"):
-    img_file_buffer_ur = r"C:\Users\dicia\NL2_project\datasets\downsampled\downsampled_2\upper_right.tif"
-    img_file_buffer_lr = r"C:\Users\dicia\NL2_project\datasets\downsampled\downsampled_2\bottom_right.tif"
-    img_file_buffer_ll = r"C:\Users\dicia\NL2_project\datasets\downsampled\downsampled_2\bottom_left.tif"
-    img_file_buffer_ul = r"C:\Users\dicia\NL2_project\datasets\downsampled\downsampled_2\upper_left.tif"
-
 for i in range(len(files)):
     # DEBUGGING
     # create folder
@@ -80,6 +65,28 @@ for i in range(len(files)):
         print(f"Cartella '{save_dir_image_i}' creata con successo")
     except OSError as e:
         print(f"Errore nella creazione della cartella: {e}")
+
+## App description
+start_time = time.time()
+
+## Upload images and rotate them by given angle
+if os.path.exists("/mnt/Volume/Mega/LaureaMagistrale/CorsiSemestre/A2S1/MultudisciplinaryProject/StitchPro/test-data/"):
+    img_file_buffer_ur = "/mnt/Volume/Mega/LaureaMagistrale/CorsiSemestre/A2S1/MultudisciplinaryProject/data/Dataset_07/upper_right.tif"
+    img_file_buffer_lr = "/mnt/Volume/Mega/LaureaMagistrale/CorsiSemestre/A2S1/MultudisciplinaryProject/data/Dataset_07/bottom_right.tif"
+    img_file_buffer_ll = "/mnt/Volume/Mega/LaureaMagistrale/CorsiSemestre/A2S1/MultudisciplinaryProject/data/Dataset_07/bottom_left.tif"
+    img_file_buffer_ul = "/mnt/Volume/Mega/LaureaMagistrale/CorsiSemestre/A2S1/MultudisciplinaryProject/data/Dataset_07/upper_left.tif"
+elif os.path.exists(r"C:\Users\dicia\NL2_project\datasets\test-data-corretto"):
+    img_file_buffer_ur = r"C:\Users\dicia\NL2_project\datasets\downsampled\downsampled_2\upper_right.tif"
+    img_file_buffer_lr = r"C:\Users\dicia\NL2_project\datasets\downsampled\downsampled_2\bottom_right.tif"
+    img_file_buffer_ll = r"C:\Users\dicia\NL2_project\datasets\downsampled\downsampled_2\bottom_left.tif"
+    img_file_buffer_ul = r"C:\Users\dicia\NL2_project\datasets\downsampled\downsampled_2\upper_left.tif"
+elif os.path.exists("/kaggle/input/"):
+    img_file_buffer_ur = '/kaggle/input/dataset/Dataset_07/upper_right.tif'
+    img_file_buffer_ul = '/kaggle/input/dataset/Dataset_07/upper_left.tif'
+    img_file_buffer_ll = '/kaggle/input/dataset/Dataset_07/bottom_left.tif'
+    img_file_buffer_lr = '/kaggle/input/dataset/Dataset_07/bottom_right.tif'
+
+
 
 
 if (img_file_buffer_ur is not None) & (img_file_buffer_lr is not None) & (img_file_buffer_ll is not None) & (
@@ -603,8 +610,8 @@ if (img_file_buffer_ur is not None) & (img_file_buffer_lr is not None) & (img_fi
                 no_nearby_y = no_nearby_y / point_distance.shape[1]
                 hist_loss += h + (no_nearby_x + no_nearby_y) * max_dist
 
-            print("mis_loss: ", mis_loss)
-            print("hist_loss: ", hist_loss)
+            # print("mis_loss: ", mis_loss)
+            # print("hist_loss: ", hist_loss)
             loss = (mis_loss) * alpha + (1 - alpha) * hist_loss
             return loss
 
@@ -719,17 +726,13 @@ if (img_file_buffer_ur is not None) & (img_file_buffer_lr is not None) & (img_fi
 
         # Save plot
         plt.savefig(os.path.join(root_folder, 'debug', 'Andamento_loss.png'))
-        plt.show()
+        # plt.show()
 
-        # output_size = max([max(x.shape) for x in images_original]) * 2
-        output_size = max([max(x.shape) for x in images_original])
+        output_size = max([max(x.shape) for x in images_original]) * 2
+        # output_size = max([max(x.shape) for x in images_original])
         output_size = [output_size, output_size]
         output = np.zeros((*output_size, 3), dtype=np.int32)
         output_d = np.zeros((*output_size, 3), dtype=np.int32)
-
-        plt.figure()
-        plt.imshow(output_d)
-        plt.savefig(os.path.join(root_folder, 'debug', 'pre_output_d0_custom.png'))
 
         sc = np.array(
             [[1, 1, DOWNSAMPLE_LEVEL], [1, 1, DOWNSAMPLE_LEVEL], [0, 0, 1]])
@@ -785,9 +788,10 @@ if (img_file_buffer_ur is not None) & (img_file_buffer_lr is not None) & (img_fi
                 output_d[im[:, :, :] > 0] += 1
 
         # DEBUGGING
-        plt.figure()
-        plt.imshow(output)
-        plt.savefig(os.path.join(root_folder, 'debug', 'pre_output0_custom.png'))
+        # plt.figure()
+        # plt.imshow(output)
+        # plt.savefig(os.path.join(root_folder, 'debug', 'pre_output0_custom.png'))
+        plt.imsave(os.path.join(root_folder, 'debug', 'pre_output0_custom.png'), output.astype('uint8'))
 
         # Converti output_d in un'immagine a singolo canale sommando lungo l'asse del colore (RGB)
         output_d_single_channel = np.sum(output_d, axis=2)
@@ -797,6 +801,9 @@ if (img_file_buffer_ur is not None) & (img_file_buffer_lr is not None) & (img_fi
         plt.imshow(output_d_single_channel, cmap='hot')  # Usa una colormap per evidenziare i valori
         plt.colorbar()  # Aggiungi una barra per visualizzare la scala dei valori
         plt.savefig(os.path.join(root_folder, 'debug', 'output_d_colormap_fixed.png'))
+        # plt.close()
+
+
 
         print("Ho finito il ciclo")
 
@@ -824,24 +831,23 @@ if (img_file_buffer_ur is not None) & (img_file_buffer_lr is not None) & (img_fi
 
         # Cambio sfondo da nero a bianco
         output[output == 0] = 255
-        plt.figure()
-        plt.imshow(output)
-        plt.savefig(os.path.join(root_folder, 'debug', 'pre_output1_0_255_custom.png'))
+        # plt.figure()
+        # plt.imshow(output)
+        # plt.savefig(os.path.join(root_folder, 'debug', 'pre_output1_0_255_custom.png'))
+        plt.imsave(os.path.join(root_folder, 'debug', 'pre_output1_0_255_custom.png'), output.astype('uint8'))
 
         print(output_d.shape)
         # if output_d > 1 --> output/output_d
         # else: output_d <= 1 --> output
         # Ma siccome output_d è un'immagine nera, output rimane uguale! NO, c'è qualcosa che mi sfugge
         output = np.where(output_d > 1, output / output_d, output)
-        plt.figure()
-        plt.imshow(output)
+        # plt.figure()
+        # plt.imshow(output)
         plt.savefig(os.path.join(root_folder, 'debug', 'pre_output2_after_output-output_d.png'))
 
         # output[np.sum(output, axis = -1) > 650] = 0
         output = output.astype(np.uint8)
-        plt.figure()
-        plt.imshow(output)
-        plt.savefig(os.path.join(root_folder, 'debug', 'pre_output3_npuint8.png'))
+        plt.imsave(os.path.join(root_folder, 'debug', 'pre_output3_npuint8.png'), output)
 
         # imageio.imwrite("/Users/anacastroverde/Desktop/output_black.tif", output, format="tif")
 
