e8e76fb5709940ef3c3948d5080ec753fc312fff
davidesecco <d.secco2000@gmail.com>
2024-10-21 11:03:48 +0200

    Cancellato un print + aggiunta la possibilità di runnare tests su tutti i datasets da command line

--- total_test.py
+++ total_test.py
@@ -1,10 +1,33 @@
 import os
 import subprocess
+import argparse
+import sys
 
-if os.path.exists("/mnt/Volume/Mega/LaureaMagistrale/CorsiSemestre/A2S1/MultudisciplinaryProject/data/"):
-    datasets_directory = "/mnt/Volume/Mega/LaureaMagistrale/CorsiSemestre/A2S1/MultudisciplinaryProject/data/"
+# Possibilità 1: Passo il percorso della cartella tramite command line
+parser = argparse.ArgumentParser(description="Script per caricare e ruotare immagini")
+parser.add_argument(
+    '--datasets_directory',
+    type=str,
+    default=None,  # Se non viene fornito dall'utente, rimane None
+    help="Percorso opzionale per le immagini"
+)
+
+# Parsing degli argomenti
+args = parser.parse_args()
+
+dataset_folder_command_line = args.datasets_directory
+datasets_directory_Davide = "/mnt/Volume/Mega/LaureaMagistrale/CorsiSemestre/A2S1/MultudisciplinaryProject/dat/"
+
+print(dataset_folder_command_line)
+
+if os.path.exists(dataset_folder_command_line):
+    datasets_directory = dataset_folder_command_line
+elif os.path.exists(datasets_directory_Davide):
+    datasets_directory = datasets_directory_Davide
 else:
-    print("Non ho trovato la cartella con tutti i dataset")
+    print("Percoso inserito dei datasets non esistente")
+    sys.exit(1)
+
 
 for folder in sorted(os.listdir(datasets_directory)):
     dataset_dir = os.path.join(datasets_directory, folder)
